146eee548ba09f12d0ddece986ff54c2d3d71a61
ana-dav <opa.super.op@gmail.com>
2020-10-19 21:31:39 +0300

    add dream team solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,8 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
+  //if(+sampleActivity.isNaN) return false
+  //sampleActivity = MODERN_ACTIVITY - HALF_LIFE_PERIOD
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
